4ad4e6fbeb3e4ea82142d61bcd4da8cae90f0f13
daryldcL <daryl.lomugdang@gmail.com>
2017-04-20 13:43:55 +0800

    Second commit

---
+++ ChainOfResponsibilityPattern/ConsoleLogger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibilityPattern
+{
+    public class ConsoleLogger : AbstractLogger
+    {
+
+        public ConsoleLogger(int level)
+        {
+            this.level = level;
+        }
+
+        protected override void write(String message)
+        {
+            Console.WriteLine("Standard Console::Logger: " + message);
+        }
+    }
+}
---
+++ ChainOfResponsibilityPattern/ErrorLogger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibilityPattern
+{
+    public class ErrorLogger : AbstractLogger
+    {
+
+        public ErrorLogger(int level)
+        {
+            this.level = level;
+        }
+
+        protected override void write(String message)
+        {
+            Console.WriteLine("Error Console::Logger: " + message);
+        }
+    }
+}
